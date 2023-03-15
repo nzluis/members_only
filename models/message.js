95097fa253e83fbeba4ef46b7f3a9c16a045c50b
luis <luisnavarroFS@gmail.com>
2023-03-15 13:25:50 +0300

    choose dateTime output

--- models/message.js
+++ models/message.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
     title: { type: String, required: true},
     user: { type: Schema.Types.ObjectId, ref: "User", required: true},
-    timestamp: { type: Date, default: Date.now },
+    timestamp: { type: Date, default: Date.now, required: true},
     text: { type: String, required: true}
 })
 
@@ -13,5 +14,7 @@ MessageSchema.virtual("url").get(function(){
 })
 
 MessageSchema.virtual("timestamp_formatted").get(function(){
-    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED)
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_MED)
 })
+
+module.exports = mongoose.model("Message", MessageSchema)
